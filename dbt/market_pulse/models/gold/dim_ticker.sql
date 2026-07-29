441678a37e260b6037592b7612cb4b1f6bdae85f
Minh Pham <mphamnhat2006@gmail.com>
2026-07-29 11:06:42 +0700

    test(dbt): fail unmapped tickers in gold

--- dbt/market_pulse/models/gold/dim_ticker.sql
+++ dbt/market_pulse/models/gold/dim_ticker.sql
@@ -1,10 +1,25 @@
 select distinct
     ticker,
     case
-        when ticker in ('AAPL','MSFT','NVDA','GOOGL','META') then 'Technology'
-        when ticker in ('JPM','BAC','V','MA')                then 'Financials'
-        when ticker in ('XOM')                               then 'Energy'
-        when ticker in ('JNJ','PG','KO','PEP','WMT','COST','HD','DIS','AMZN','TSLA') then 'Consumer'
-        else 'Other'
+        when
+            ticker in ('AAPL', 'MSFT', 'NVDA', 'GOOGL', 'META')
+            then 'Technology'
+        when ticker in ('JPM', 'BAC', 'V', 'MA') then 'Financials'
+        when ticker in ('XOM') then 'Energy'
+        when
+            ticker in (
+                'JNJ',
+                'PG',
+                'KO',
+                'PEP',
+                'WMT',
+                'COST',
+                'HD',
+                'DIS',
+                'AMZN',
+                'TSLA'
+            )
+            then 'Consumer'
+        else 'UNMAPPED'
     end as sector
 from {{ ref('stg_prices') }}
