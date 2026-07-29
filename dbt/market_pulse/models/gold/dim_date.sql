83da8246a9af03790470545c0c36e235376992c3
Minh Pham <mphamnhat2006@gmail.com>
2026-07-29 11:06:49 +0700

    style(dbt): format trino models

--- dbt/market_pulse/models/gold/dim_date.sql
+++ dbt/market_pulse/models/gold/dim_date.sql
@@ -1,6 +1,6 @@
 select distinct
     trade_date,
-    year(trade_date)         as year,
-    month(trade_date)        as month,
-    day_of_week(trade_date)  as day_of_week
+    year(trade_date) as year_num,
+    month(trade_date) as month_num,
+    day_of_week(trade_date) as day_of_week
 from {{ ref('stg_prices') }}
